e7ea73c246b7ee684f1cff624e2e634f5dc20509
Dinuki19 <wdkkurera@students.nsbm.ac.lk>
2025-08-04 22:26:59 +0530

    add some client pages

--- src/pages/client/clientPage.jsx
+++ src/pages/client/clientPage.jsx
@@ -1,19 +1,55 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "../../components/header";
+import ProductsPage from "./productsPage";
+import ProductOverViewPage from "./productOverView";
+import CartPage from "./cart";
+import CheckoutPage from "./checkoutPage";
 
 export default function ClientWebPage() {
-	return <div className="w-full h-screen max-h-screen">
-        <Header/>
-        <div className="w-full h-[calc(100%-100px)] bg-yellow-400">
-            <Routes path="/">
-                <Route path="/" element={<h1 className="text-3xl text-center">Welcome to the Home Page</h1>}/>
-                <Route path="/products" element={<h1 className="text-3xl text-center">Products Page</h1>}/>
-                <Route path="/reviews" element={<h1 className="text-3xl text-center">Reviews Page</h1>}/>
-                <Route path="/about-us" element={<h1 className="text-3xl text-center">About Us Page</h1>}/>
-                <Route path="/contact-us" element={<h1 className="text-3xl text-center">Contact Us Page</h1>}/>
-                <Route path="/*" element={<h1 className="text-3xl text-center">404 Not Found</h1>}/>
-
-            </Routes>
-        </div>
-    </div>;
+	return (
+		<div className="w-full h-screen max-h-screen">
+			<Header />
+			<div className="w-full h-[calc(100%-100px)] ">
+				<Routes path="/">
+					<Route
+						path="/"
+						element={
+							<h1 className="text-3xl text-center">Welcome to the Home Page</h1>
+						}
+					/>
+					<Route
+						path="/products"
+						element={<ProductsPage/>}
+					/>
+					<Route
+						path="/reviews"
+						element={<h1 className="text-3xl text-center">Reviews Page</h1>}
+					/>
+					<Route
+						path="/about-us"
+						element={<h1 className="text-3xl text-center">About Us Page</h1>}
+					/>
+					<Route
+						path="/contact-us"
+						element={<h1 className="text-3xl text-center">Contact Us Page</h1>}
+					/>
+					<Route
+						path="/cart"
+						element={<CartPage/>}
+					/>
+                    <Route
+                        path="/overview/:productId"
+                        element={<ProductOverViewPage />}
+                    />
+					<Route
+						path="/checkout"
+						element={<CheckoutPage/>}/>
+					<Route
+						path="/*"
+						element={<h1 className="text-3xl text-center">404 Not Found</h1>}
+					/>
+				</Routes>
+			</div>
+		</div>
+	);
 }
